649eddc065dcb017fa49d98b4a685fc1dfec01a4
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-12-09 18:35:42 +0530

    Added mapDispatchToProps manual mapping

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -21,10 +21,8 @@ class CoursesPage extends React.Component {
     }
 
     onClickSave() {
-        this.props.dispatch(
-            courseActions.createCourse(
-                this.state.course
-            )
+        this.props.createCourse(
+            this.state.course
         );
     }
 
@@ -53,8 +51,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    courses: PropTypes.array.isRequired
+    courses: PropTypes.array.isRequired,
+    createCourse: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -63,4 +61,10 @@ function mapStateToProps(state, ownProps) {
     };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+    return {
+        createCourse: course => dispatch(courseActions.createCourse(course))
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
