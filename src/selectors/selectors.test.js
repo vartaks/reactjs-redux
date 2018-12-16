a8782386c9ef327be65834bc01ac5edf5af352bb
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-12-16 17:35:39 +0530

    Fixed formatting

--- src/selectors/selectors.test.js
+++ src/selectors/selectors.test.js
@@ -1,5 +1,5 @@
 import expect from 'expect';
-import {authorsFormattedForDropdown} from './selectors';
+import { authorsFormattedForDropdown } from './selectors';
 
 describe('Author Selectors', () => {
     describe('authorsFormattedForDropdown', () => {
@@ -9,23 +9,23 @@ describe('Author Selectors', () => {
                     id: 'cory-house',
                     firstName: 'Cory',
                     lastName: 'House'
-                  },
-                  {
+                },
+                {
                     id: 'scott-allen',
                     firstName: 'Scott',
                     lastName: 'Allen'
-                  }
+                }
             ];
 
             const expected = [
                 {
                     value: 'cory-house',
                     text: 'Cory House'
-                  },
-                  {
+                },
+                {
                     value: 'scott-allen',
                     text: 'Scott Allen'
-                  }
+                }
             ];
 
             expect(authorsFormattedForDropdown(authors)).toEqual(expected);
