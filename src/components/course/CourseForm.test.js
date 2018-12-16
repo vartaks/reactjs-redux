f98a1d8e6b085b098a852ab09bb70ed46d636103
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-12-16 16:34:06 +0530

    Added more test cases

--- src/components/course/CourseForm.test.js
+++ src/components/course/CourseForm.test.js
@@ -3,17 +3,18 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import CourseForm from './CourseForm';
 
-function setup() {
+function setup(saving) {
     let props = {
-        course: {}, saving: false, errors: {},
+        allAuthors: [],
+        course: {},
+        saving: saving,
+        errors: {},
         onSave: () => { },
         onChange: () => { }
     };
 
     let renderer = TestUtils.createRenderer();
-
     renderer.render(<CourseForm {...props} />);
-
     let output = renderer.getRenderOutput();
 
     return {
@@ -29,5 +30,17 @@ describe('CourseForm via React Test Utils', () => {
         expect(output.type).toBe('form');
         let [h1] = output.props.children;
         expect(h1.type).toBe('h1');
+    });
+
+    it('save button is labeled "Save" when not saving', () => {
+        const { output } = setup(false);
+        const submitButton = output.props.children[5];
+        expect(submitButton.props.value).toBe('Save');
+    });
+
+    it('save button is labeled "Saving..." when saving', () => {
+        const { output } = setup(true);
+        const submitButton = output.props.children[5];
+        expect(submitButton.props.value).toBe('Saving...');
     });
 });
