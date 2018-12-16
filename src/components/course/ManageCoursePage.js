4bd4281cc22ff93da15d3be86278ff704392cbb3
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-12-16 14:49:03 +0530

    Fixed warning "Using exported name 'ManageCoursePage' as identifier for default export  import/no-named-as-default"

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -6,7 +6,7 @@ import CourseForm from './CourseForm';
 import { authorsFormattedForDropdown } from '../../selectors/selectors';
 import toastr from 'toastr';
 
-export class ManageCoursePage extends React.Component {
+class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
