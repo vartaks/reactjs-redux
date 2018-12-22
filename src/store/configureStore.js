545cea68763f854ff6d75898fa789beaa7642fb5
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-12-22 21:49:02 +0530

    Added configuration for production build

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,8 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
+// Since dynamic imports aren't supported by ES6,
+// we are using require instead of import
 
-export default function configureStore(initialState) {
-    return createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
-    );
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./configureStore.prod');
+} else {
+    module.exports = require('./configureStore.dev');
 }
