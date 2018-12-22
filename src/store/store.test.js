39b488655539633fbe4a362c1910acca80a7444b
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-12-22 19:59:27 +0530

    Added integration test for updating courses

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -24,4 +24,38 @@ describe('Store', () => {
 
         expect(actual).toEqual(expected);
     });
+
+    it('should handle updating courses', () => {
+        // arrange
+        const store = createStore(rootReducer, initialState);
+        const courses = [{
+            id: "A",
+            title: "Clean Code"
+        },
+        {
+            id: "B",
+            title: "React Flux"
+        }];
+
+        // act
+        const actionCreateA = courseActions.createCourseSuccess(courses[0]);
+        const actionCreateB = courseActions.createCourseSuccess(courses[1]);
+        store.dispatch(actionCreateA);
+        store.dispatch(actionCreateB);
+
+        const updatedCourse = {
+            id: "B",
+            title: "React Redux"
+        };
+
+        const actionUpdate = courseActions.updateCourseSuccess(updatedCourse);
+        store.dispatch(actionUpdate);
+
+        // assert
+        const actual = store.getState().courses;
+
+        expect(actual.length).toEqual(2);
+        expect(actual[0].title).toEqual('Clean Code');
+        expect(actual[1].title).toEqual('React Redux');
+    });
 });
